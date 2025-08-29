ce4ee4e24e9fdab1ec18dd98c0c3ece9df9c13d6
Hari G <sakthihariharan007@gmail.com>
2025-08-29 16:20:26 +0530

    disable img

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export', // Enables static export
+  images: {
+    unoptimized: true, // 👈 required for static hosting
+  },
 };
 
 export default nextConfig;
