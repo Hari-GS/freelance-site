66dbe8674cf24d3dd55c306da9cea0b435bbb4cc
Hari G <sakthihariharan007@gmail.com>
2025-08-30 13:52:59 +0530

    removed static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // Enables static export
   images: {
     unoptimized: true, // 👈 required for static hosting
   },
