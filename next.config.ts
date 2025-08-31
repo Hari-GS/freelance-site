ecc5a92c1282ad7a37ddfaf145605fc1bcf4b4bd
Hari G <sakthihariharan007@gmail.com>
2025-08-31 07:33:05 +0530

    change img config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    unoptimized: true, // 👈 required for static hosting
-  },
+
 };
 
 export default nextConfig;
